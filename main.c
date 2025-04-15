d1e4237e8b7567ee8950cc572e71f503ae7996e9
devpsiarch <hmayoubaty@gmail.com>
2025-04-15 22:32:30 +0100

    fix + test

--- main.c
+++ main.c
@@ -23,7 +23,10 @@ TensorR* init_tensorR(unsigned int depth,...){
     return NULL;
 }
 /* i need to think more about the above */
-static const int iterations = 500;
+static const int iterations = 10000;
+// UNSIGNED_CENTROID is the max size_t that signifies that it is not assigned 
+// to any centroid , mostly safe .... 
+#define UNSIGNED_CENTROID (size_t)-1
 /* I want to make an abtract data */
 typedef struct VectorR VectorR;
 struct VectorR {
@@ -44,6 +47,7 @@ VectorR* init_VectorR(const unsigned int dim,...){
     result->values = malloc(sizeof(double)*dim);
     ASSERT_ALLOC(result->values);
     result->dimension = dim;
+    result->centroid = UNSIGNED_CENTROID;
     
     va_list args;
     va_start(args,dim);
@@ -55,7 +59,7 @@ VectorR* init_VectorR(const unsigned int dim,...){
     return result;
 }
 /* sets all values of said VectorR to 0 very usefull mind you...*/
-void zeroVectorR(VectorR*x){
+void zeroVectorR(VectorR*const x){
     assert(x != NULL);
     assert(x->dimension != 0);
     for(size_t i = 0 ; i < x->dimension ; i++){
@@ -63,25 +67,26 @@ void zeroVectorR(VectorR*x){
     }
 }
 /* frees a single VectorR and its content*/
-void clean_VectorR(VectorR*x){
+void clean_VectorR(VectorR*const x){
     free(x->values);
     free(x);
 }
 /* copies a VectorR from anther*/ 
-void VectorRcpy(VectorR* src,VectorR* dst){
+void VectorRcpy(VectorR*const dst,VectorR*const src){
+    assert(src != NULL);
     assert(src->dimension == dst->dimension);
-    for(size_t i = 0 ; i < src->dimension ; i++){
+    for(size_t i = 0 ; i < dst->dimension ; i++){
         dst->values[i] = src->values[i];
     }
 }
 /* frees a array of VectorR */
-void clean_bunch_VectorR(VectorR**arr,size_t n){
+void clean_bunch_VectorR(VectorR*const arr[],size_t n){
     for(size_t i = 0 ; i < n ; ++i){
         clean_VectorR(arr[i]);
     }
 }
 /* Gets distance of two points from any dimension */
-double distanceR(VectorR* a,VectorR* b){
+double distanceR(const VectorR* a,const VectorR* b){
     assert(a->dimension == b->dimension);
     double sum = 0;
     for(size_t i = 0 ; i < a->dimension ; i++){
@@ -90,24 +95,25 @@ double distanceR(VectorR* a,VectorR* b){
     return sqrt(sum);
 }
 /* prints a VectorR in the format (x1,...,xn)*/
-void printVectorR(VectorR*x){
-    printf("dim (%zu)(",x->dimension);
+void printVectorR(const VectorR*const x){
+    printf("dim (%zu) ",x->dimension);
+    if(x->centroid == UNSIGNED_CENTROID) printf("cen (unsigned) [");
+    else printf("cen (%zu) [",x->centroid);
     for(size_t i = 0 ; i < x->dimension ;++i){
         printf("%lf",x->values[i]);
         if(i != x->dimension-1) printf(",");
     }
-    printf(")\n");
+    printf("]\n");
 }
 /*Returns true of an array of a vectors are from the same size*/
-bool validateVectorR(VectorR**dataset,size_t n,size_t dim){
+bool validateVectorR(VectorR*const dataset[],size_t n,size_t dim){
     for(size_t i = 0 ; i < n ; i++){
-        printf("dimension => %zu\n",dataset[i]->dimension);
         if(dataset[i]->dimension != dim) return false;
     }
     return true;
 }
 /* add up two matrices , perfomrs '+=' */
-void addupcumVectorR(VectorR*dst,VectorR*src){
+void addupcumVectorR(VectorR*const dst,VectorR*const src){
     assert(dst != NULL);
     assert(dst->dimension != 0);
     assert(dst->dimension == src->dimension);
@@ -116,7 +122,7 @@ void addupcumVectorR(VectorR*dst,VectorR*src){
     }
 }
 /* scales a VectorR by some scaler 'double' changes are applied to the vector itself*/
-void scalVectorR(VectorR*dst,double scaler){
+void scalVectorR(VectorR* const dst,double scaler){
     assert(dst != NULL);
     assert(dst->dimension != 0);
     for(size_t i = 0 ; i < dst->dimension ; i++){
@@ -124,12 +130,18 @@ void scalVectorR(VectorR*dst,double scaler){
     }
 }
 /*Kmeans for any 'stable' dataset of the same dimension*/
-void KmeansR(VectorR** dataset,size_t n,size_t k){
+void KmeansR(VectorR* dataset[],size_t n,size_t k){
     // assert that the dataset are of the same dimension
     assert(dataset != NULL);
-    assert(validateVectorR(dataset,n,dataset[0]->dimension));
+    size_t dim = dataset[0]->dimension;
+    assert(validateVectorR(dataset,n,dim));
     // create centroids as array of vectors (multi dimension).
     VectorR* centroids = malloc(sizeof(VectorR)*k);
+    // init the dimentions of the centroids since they are not
+    for(size_t i = 0 ; i < k; i++){
+        centroids[i].values = malloc(sizeof(double) * dim);
+        centroids[i].dimension = dim;
+    }
     // init values to centroids
     for(size_t i = 0 ; i < k ; i++){
         VectorRcpy(&centroids[i],dataset[rand()%n]);
@@ -155,7 +167,7 @@ void KmeansR(VectorR** dataset,size_t n,size_t k){
         /* UPDATE STEP */
         // getting the mean for each centroid
         for(size_t c = 0 ; c < k ; c++){
-            VectorR* sum = init_VectorR(k,0);
+            VectorR* sum = init_VectorR(dim,0);
             zeroVectorR(sum);
             size_t count = 0;
             for(size_t p = 0 ; p < n ; p++){
@@ -176,7 +188,7 @@ void KmeansR(VectorR** dataset,size_t n,size_t k){
             clean_VectorR(sum);
         }
     }
-    clean_bunch_VectorR(&centroids,k);
+    clean_VectorR(centroids);
 }
 
 typedef struct point point;
@@ -249,15 +261,27 @@ void print_data(point* points,size_t n){
 }
 int main(void){
     srand(time(0));
-    VectorR* test[] = { 
-        init_VectorR(1,1.f),
-        init_VectorR(1,2.f),
-        init_VectorR(1,3.f),
-        init_VectorR(1,4.f),
-    };
-    printVectorR(test[0]);
-    clean_bunch_VectorR(test,4);
-    KmeansR(test,4,2);
+    size_t data_size = 8;
+
+    VectorR* test[8];
+    test[0] = init_VectorR(2, -100.f, -20.f);
+    test[1] = init_VectorR(2, -2.5f, -50.f);
+    test[2] = init_VectorR(2, 5.f, 30.f);
+    test[3] = init_VectorR(2, 4.f, 2.f);
+    test[4] = init_VectorR(2, 5.2f, 31.f);
+    test[5] = init_VectorR(2, 4.1f, 3.f);
+    test[6] = init_VectorR(2, -3.f, -49.f);
+    test[7] = init_VectorR(2, -99.f, -19.5f); 
+
+    for (size_t i = 0 ; i < data_size;++i) {
+        printVectorR(test[i]);
+    }
+    printf("---------------------------\n");
+    KmeansR(test,data_size,3);
+    for (size_t i = 0 ; i < data_size;++i) {
+        printVectorR(test[i]);
+    }
+    clean_bunch_VectorR(test,data_size);
     return 0;
     // point dataset[5] = {
     //     {150,0},
